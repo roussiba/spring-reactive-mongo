d7ce9b703c6cb8473772a822930b165ed5e7b0dc
bassirou Diakhate <roussibadiakhate@hotmail.fr>
2024-02-28 22:19:04 +0000

    jdhd

--- src/test/java/com/genisys/springreactivemongo/SpringReactiveMongoApplicationTests.java
+++ src/test/java/com/genisys/springreactivemongo/SpringReactiveMongoApplicationTests.java
@@ -3,9 +3,11 @@ package com.genisys.springreactivemongo;
 import com.genisys.springreactivemongo.dto.ProductDto;
 import com.genisys.springreactivemongo.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -13,9 +15,10 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
-
+@RunWith(SpringRunner.class)
 @WebFluxTest
 class SpringReactiveMongoApplicationTests {
 
@@ -74,6 +77,15 @@ class SpringReactiveMongoApplicationTests {
                  .expectSubscription()
                  .expectNextMatches(p -> p.name().equals("mobile"))
                  .verifyComplete();
+    }
+
+    @Test
+    public void deleteProductTest(){
+        given(productService.deleteProduct(any())).willReturn(Mono.empty());
+
+        webTestClient.delete().uri("/products/120")
+                .exchange()
+                .expectStatus().isOk();
     }
 
 }
